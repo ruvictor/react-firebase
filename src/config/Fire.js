7c274fcb0f11a60389258cd48fa4010711b6b72f
Victor <rusuvictor30@gmail.com>
2019-06-02 21:02:26 -0400

    Final cleanup

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyCLd66Ub-WmGSc5CLrXD5-8fllwzKvwk1g",
-    authDomain: "react-firebase-fad93.firebaseapp.com",
-    databaseURL: "https://react-firebase-fad93.firebaseio.com",
-    projectId: "react-firebase-fad93",
-    storageBucket: "react-firebase-fad93.appspot.com",
-    messagingSenderId: "629714215230",
-    appId: "1:629714215230:web:b77ecc29ec775221"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const fire = firebase.initializeApp(config);
